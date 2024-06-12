56044ba5d17d20ea8515c0743ae0851d658954d0
Cielettee <tlbecker08@gmail.com>
2024-06-12 00:23:34 -0700

    Added Back to Home Button
    Much needed, very infuriating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,16 @@
     import '../app.css'
     import '@fortawesome/fontawesome-free/css/all.min.css'
     import { base } from '$app/paths'
+    import noseguy from '$lib/assets/nose.png'
 </script>
 
 <div class="bg-[#1A120B]">
-    <div class="grid grid-cols-8 grid-flow-row w-full h-screen h-full">
+    <div class="grid grid-cols-1 md:grid-cols-8 grid-flow-row w-full h-screen h-full">
         <!-- Top nav bar -->
-        <div class="flex justify-center items-center h-screen w-full">
+        <div class="flex justify-center items-center md:h-screen w-full">
             <!-- Space them evenly, take up whole width -->
-            <div class="flex flex-col text-center justify-evenly h-6/10 text-[#D5CEA3] gap-8">
+            <div class="flex flex-col text-center items-center justify-evenly h-6/10 text-[#D5CEA3] gap-8">
+                <a href={base + '/'} class="py-4 px-8 rounded-lg"><img src={noseguy} class="w-[6rem] h-[6rem] rounded-full"></a>
                 <a href={base + '/aboutme'} class="text-lg font-bold hover:bg-[#3C2A21] py-4 px-8 rounded-lg">About Me!</a>
                 <a href={base + '/newstuff'} class="text-lg font-bold hover:bg-[#3C2A21] py-4 px-8 rounded-lg">Current Stuff!</a>
                 <a href={base + '/resume'} class="text-lg font-bold hover:bg-[#3C2A21] py-4 px-8 rounded-lg">Resume!</a>
--- src/routes/newstuff/+page.svelte
+++ src/routes/newstuff/+page.svelte
@@ -1,3 +1,7 @@
 <script>
    import { base } from '$app/paths'
 </script>
+
+<h1 class="text-[#D5CEA3] text-bold">
+   Stuff will be here soon! :^3
+</h1>
