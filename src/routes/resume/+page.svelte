e65fff98365c8c0bede98cff8d564f6649602f4c
Cielettee <tlbecker08@gmail.com>
2024-06-11 23:42:04 -0700

    Resume Fix

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,5 +2,4 @@
    import { base } from '$app/paths'
 </script>
 
-<embed src="https://drive.google.com/viewerng/
-viewer?embedded=true&url={base + '/resume.pdf'}" width="100%" height="100%" class='rounded-lg'/>
+<embed src="{base + '/resume.pdf'}" width="100%" height="100%" class='rounded-lg'/>
